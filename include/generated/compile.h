ef78859e70d8b18b9c6902205b6cff305240451d
km2 <gtk3@inbox.ru>
2019-11-25 20:31:07 +0500

    Коммит ядра 3.10.72

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon May 25 20:21:59 EEST 2015"
+#define LINUX_COMPILE_BY "valik1410valik"
+#define LINUX_COMPILE_HOST "Valentin-PC"
+#define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (Linaro GCC 2014.11) "
